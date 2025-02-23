44ef0b0930d0e70f4120e6f5ae5b37fbe0881807
Harihar Prasad <137610971+HariharPrasadd@users.noreply.github.com>
2025-02-23 20:22:25 +0530

    Solved 6th exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
-
+const leapYears = function(input) {
+    if(input%100 === 0){
+        if(input%400 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    if(input%4 === 0){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+declare number input. 
+if input is divisible by 100
+    if input is divisible by 400
+        input is a leap year
+    else
+        input is not a leap year
+if input is divisible by 4
+    input is a leap year
+*/
