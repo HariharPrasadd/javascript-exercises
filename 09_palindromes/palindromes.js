8dcc9fbd648b9aee3799fc11e2b88e1b87c66d90
Harihar Prasad <137610971+HariharPrasadd@users.noreply.github.com>
2025-03-03 22:03:52 +0530

    Solved 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase();
+    let inputArray = input.split('');
+    let inputChar = inputArray.filter(isLetterOrNumber);
+    let original = inputChar.join('');
+    let reverseChar = inputChar.reverse();
+    if(original === reverseChar.join('')){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
+
+function isLetterOrNumber(c) {
+    if(c.toLowerCase() != c.toUpperCase() || (c >= '0' && c <= '9')){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
