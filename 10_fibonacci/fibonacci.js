3102901d3b4d1c3737c1948ce064ada43d984c54
Harihar Prasad <137610971+HariharPrasadd@users.noreply.github.com>
2025-03-03 22:12:51 +0530

    Solved 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    if(+fib < 0){
+        return "OOPS"
+    }
+    else{
+        if(+fib == 0){
+            return 0;
+        }
+        else if(+fib == 1){
+            return 1;
+        }
+        else{
+            return fibonacci(+fib - 1) + fibonacci(+fib - 2);
+        }
+    }
 };
+
+console.log(fibonacci(25))
 
 // Do not edit below this line
 module.exports = fibonacci;
