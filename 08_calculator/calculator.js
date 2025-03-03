5e637cfd4dd0aa739d9217356039553179de7ef9
Harihar Prasad <137610971+HariharPrasadd@users.noreply.github.com>
2025-03-03 20:24:20 +0530

    Solved 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,32 @@
 const add = function() {
-	
+  return [...arguments].flat().reduce((sum, num) => {return sum + num;}, 0);
 };
 
 const subtract = function() {
-	
+  return [...arguments].flat().reduce((sum, num) => {return sum - num;});
 };
 
 const sum = function() {
-	
+  return [...arguments].flat().reduce((sum, num) => {return sum + num;}, 0);
 };
 
 const multiply = function() {
-
+  return [...arguments].flat().reduce((sum, num) => {return sum * num;}, 1);
 };
 
 const power = function() {
-	
+	return [...arguments].flat().reduce((sum, num) => {return sum**num;});
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let factorial = 1;
+  for(let i = 1; i <= fact; i++){
+    factorial *= i;
+  }
+  return factorial;
 };
 
+console.log(add([7]));
 // Do not edit below this line
 module.exports = {
   add,
